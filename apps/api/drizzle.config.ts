9ac95d9c82c6657340178975652fbffc99659691
Palash Chanda <palash@itobuz.com>
2026-08-12 16:00:03 +0530

    chore: api env made dynamic

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -1,6 +1,12 @@
 /// <reference types="node" />
-import 'dotenv/config';
+import dotenv from 'dotenv';
 import type { Config } from 'drizzle-kit';
+import path from 'node:path';
+
+const environment = process.env.NODE_ENV || 'development';
+const envPath = path.resolve(`./apps/api/.env.${environment}`);
+
+dotenv.config({ path: envPath });
 
 export default {
   schema: './src/db/schema/index.ts',
